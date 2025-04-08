dd5d6232e82971ecfbc62b5575bdb36c98e0a3e5
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-07 20:28:30 -0500

    Agregar servicio Summary con método para obtener publicaciones totales por usuario

---
+++ src/app/core/services/summary/summary.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SummaryService {
+
+  private apiSummary = environment.apiSummary;
+  constructor(private httpClient: HttpClient) { }
+  public getTotalCareerPublicationUser(): Observable<any> {
+		return this.httpClient.get(`${this.apiSummary}`);
+	}
+}
