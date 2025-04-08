d20f50581628935816e81d51481297807fa7d06f
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-07 20:28:16 -0500

    Agregar ruta para el componente de carrera en las rutas de la aplicación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/oauth/login/login.component';
 import { RegisterComponent } from './pages/oauth/register/register.component';
+import { CareerComponent } from './pages/career/career.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'carrera/:idCarrera', component: CareerComponent },
+
     { path: '**', redirectTo: '' }
 
 ];
