048ab2c3e2d4ec4adee0ca1cea34766d6065a3f8
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-07 20:29:05 -0500

    Agregar componentes Summary y Publication al componente Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,13 @@ import { app } from '../../../../server';
 import { HeaderComponent } from "../../shared/components/header/header.component";
 import { HeroComponent } from './components/hero/hero.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { SummaryComponent } from '../../shared/components/summary/summary.component';
+import { PublicationComponent } from '../../shared/components/publication/publication.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,HeroComponent,FooterComponent],
+  imports: [HeaderComponent,HeroComponent,FooterComponent,SummaryComponent,PublicationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
