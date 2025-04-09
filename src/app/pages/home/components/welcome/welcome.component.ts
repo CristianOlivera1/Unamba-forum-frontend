aaeafcb52a2fbbd41ccb5ea30bffcee1463d0d88
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-08 20:40:16 -0500

    Refactor code structure for improved readability and maintainability

--- src/app/pages/home/components/welcome/welcome.component.ts
+++ src/app/pages/home/components/welcome/welcome.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenService } from '../../../../core/services/oauth/token.service';
+import { ProfileService } from '../../../../core/services/profile/profile.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
 
+  constructor(
+    private tokenService: TokenService,
+    private profileService: ProfileService
+  ) {}
+  isLoggedIn: boolean = false;
+  userProfile: any = null;
+  ngOnInit(): void {
+    if (typeof window !== 'undefined') {
+      this.isLoggedIn = !!this.tokenService.getToken();
+      if (this.isLoggedIn) {
+        this.loadUserProfile();
+      }
+  
+      // Escucha cambios en localStorage
+      window.addEventListener('storage', () => {
+        this.isLoggedIn = !!this.tokenService.getToken();
+        if (this.isLoggedIn) {
+          this.loadUserProfile();
+        } else {
+          this.userProfile = null;
+        }
+      });
+    }
+  }
+
+
+  loadUserProfile(): void {
+    const userId = this.extractUserIdFromToken();
+    if (userId) {
+      this.profileService.getProfileByUserId(userId).subscribe(
+        (response) => {
+          if (response.type === 'success') {
+            this.userProfile = response.data;
+          } else {
+            console.error('Error al obtener el perfil:', response.listMessage);
+          }
+        },
+        (error) => {
+          console.error('Error en la solicitud del perfil:', error);
+        }
+      );
+    }
+  }
+
+  extractUserIdFromToken(): string | null {
+    const token = this.tokenService.getToken();
+    if (token) {
+      const payload = JSON.parse(atob(token.split('.')[1])); // Decodifica el payload del JWT
+      return payload.idUsuario || null;
+    }
+    return null;
+  }
 }
--- src/app/shared/components/publication/publication.component.ts
+++ src/app/shared/components/publication/publication.component.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { WelcomeComponent } from '../../../pages/home/components/welcome/welcome.component';
+import { TokenService } from '../../../core/services/oauth/token.service';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-publication',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,WelcomeComponent,ButtonModule],
   templateUrl: './publication.component.html',
   styleUrl: './publication.component.css'
 })
 export class PublicationComponent {
+  isOpen = false;
+  selectedOption: string | null = null;
 
+  options: string[] = [
+    'Todas',
+    'Tecnología',
+    'Ropa',
+    'Hogar',
+    'Deportes',
+  ];
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+    this.isOpen = false;
+  }
+  
+  @ViewChild('scrollContainer', { static: true }) scrollContainer!: ElementRef;
+
+  isDragging = false;
+  startX = 0;
+  scrollLeft = 0;
+  isLoggedIn: boolean = false;
+
+  constructor(private tokenService: TokenService) {}
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenService.getToken();
+  }
+
+  startDrag(event: MouseEvent | TouchEvent) {
+    this.isDragging = true;
+
+    const pageX = (event instanceof TouchEvent)
+      ? event.touches[0].pageX
+      : event.pageX;
+
+    this.startX = pageX - this.scrollContainer.nativeElement.offsetLeft;
+    this.scrollLeft = this.scrollContainer.nativeElement.scrollLeft;
+  }
+
+  stopDrag() {
+    this.isDragging = false;
+  }
+
+  onDrag(event: MouseEvent | TouchEvent) {
+    if (!this.isDragging) return;
+
+    event.preventDefault();
+
+    const pageX = (event instanceof TouchEvent)
+      ? event.touches[0].pageX
+      : event.pageX;
+
+    const x = pageX - this.scrollContainer.nativeElement.offsetLeft;
+    const walk = (x - this.startX) * 1.5;
+
+    this.scrollContainer.nativeElement.scrollLeft = this.scrollLeft - walk;
+  }
 }
