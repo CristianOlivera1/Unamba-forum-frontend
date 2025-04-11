ae0767768c7c69f1da8fe7cb64a87480b6bb5424
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-11 10:12:08 -0500

    feat: implementar lógica para agregar, actualizar y eliminar reacciones en el componente Reaction

--- src/app/pages/home/components/reaction/reaction.component.ts
+++ src/app/pages/home/components/reaction/reaction.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { ReactionPublicationService } from '../../../../core/services/reaction/reaction-publication.service';
+import { TokenService } from '../../../../core/services/oauth/token.service';
+import { ModalService } from '../../../../core/services/modal/modal.service';
 
 @Component({
   selector: 'app-reaction',
@@ -7,10 +10,111 @@ import { Component } from '@angular/core';
   templateUrl: './reaction.component.html',
   styleUrl: './reaction.component.css'
 })
-export class ReactionComponent {
+export class ReactionComponent implements OnInit {
+
+  @Input() idUsuario!: string;
+  @Input() idPublicacion!: string;
+
   popoverVisible = false;
   tooltipActivo: string | null = null;
+  currentReaction: string | null = null;
+  isLoggedIn = false;
+
   
+  constructor(private reactionService: ReactionPublicationService,    private tokenService: TokenService,private modalService: ModalService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.idUsuario && this.idPublicacion) {
+      this.loadCurrentReaction();
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['idUsuario'] || changes['idPublicacion']) {
+      if (this.idUsuario && this.idPublicacion) {
+        this.loadCurrentReaction();
+      }
+    }
+  }
+
+  loadCurrentReaction(): void {
+    this.reactionService.getReaction(this.idUsuario, this.idPublicacion).subscribe({
+      next: (response) => {
+        this.currentReaction = response?.data?.tipo || null;
+      },
+      error: (err) => {
+        console.error('Error al cargar la reacción:', err);
+      }
+    });
+  }
+
+  addReaction(tipo: string): void {
+    if (!this.tokenService.isLoggedIn()) {
+      // Mostrar el modal de inicio de sesión si el usuario no está autenticado
+      this.modalService.showLoginModal();
+      return;
+    }
+
+    if (this.currentReaction === tipo) {
+      // Si el usuario hace clic en el mismo emoji, elimina la reacción
+      this.removeReaction();
+      return;
+    }
+  
+    if (this.currentReaction) {
+      // Si ya existe una reacción, actualiza la reacción
+      this.reactionService.updateReaction(this.idUsuario, this.idPublicacion, tipo).subscribe({
+        next: () => {
+          this.currentReaction = tipo;
+          this.popoverVisible = false;
+        },
+        error: (err) => {
+          console.error('Error al actualizar la reacción:', err);
+        }
+      });
+    } else {
+      // Si no existe una reacción, inserta una nueva
+      const dtoReaction = {
+        idUsuario: this.idUsuario,
+        idPublicacion: this.idPublicacion,
+        tipo: tipo
+      };
+  
+      this.reactionService.addReaction(dtoReaction).subscribe({
+        next: () => {
+          this.currentReaction = tipo;
+          this.popoverVisible = false;
+        },
+        error: (err) => {
+          console.error('Error al agregar la reacción:', err);
+        }
+      });
+    }
+  }
+
+  removeReaction(): void {
+
+    if (!this.tokenService.isLoggedIn()) {
+      this.modalService.showLoginModal();
+      return;
+    }
+
+    if (!this.currentReaction) {
+      this.addReaction('Me identifica');
+      return;
+    }
+  
+    this.reactionService.removeReaction(this.idUsuario, this.idPublicacion).subscribe({
+      next: () => {
+        this.currentReaction = null;
+      },
+      error: (err) => {
+        console.error('Error al eliminar la reacción:', err);
+      }
+    });
+  }
+
   mostrarPopover() {
     this.popoverVisible = true;
   }
