cce9e4ca5f2f0990b9962ce66d7c4c8f4ea4ed53
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-11 10:11:33 -0500

    feat: agregar servicio ReactionPublication para gestionar reacciones a publicaciones

---
+++ src/app/core/services/reaction/reaction-publication.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReactionPublicationService {
+
+  private apiReactionPublication = environment.apiReactionPublication;
+
+  constructor(private httpClient: HttpClient) {}
+  addReaction(dtoReaction: any): Observable<any> {
+    const url = `${this.apiReactionPublication}/insert`;
+    const formData = new FormData();
+    formData.append('idUsuario', dtoReaction.idUsuario);
+    formData.append('idPublicacion', dtoReaction.idPublicacion);
+    formData.append('tipo', dtoReaction.tipo);
+  
+    return this.httpClient.post<any>(url, formData);
+  }
+
+  removeReaction(idUsuario: string, idPublicacion: string): Observable<any> {
+    const url = `${this.apiReactionPublication}/delete`;
+    const params = { idUsuario, idPublicacion };
+    return this.httpClient.delete<any>(url, { params });
+  }
+
+  /*Cargar las reacciones del usuario logueado a todas las publicaciones */
+  getReaction(idUsuario: string, idPublicacion: string): Observable<any> {
+    const url = `${this.apiReactionPublication}/get`;
+    const params = { idUsuario, idPublicacion };
+    return this.httpClient.get<any>(url, { params });
+  }
+
+  updateReaction(idUsuario: string, idPublicacion: string, nuevoTipo: string): Observable<any> {
+    const url = `${this.apiReactionPublication}/update`;
+    const params = { idUsuario, idPublicacion, nuevoTipo };
+    return this.httpClient.put<any>(url, null, { params });
+  }
+
+  getReactionAndCommentSummary(idPublicacion: string): Observable<any> {
+    const url = `${this.apiReactionPublication}/reactioncommentsummary`;
+    const params = { idPublicacion };
+    return this.httpClient.get<any>(url, { params });
+  }
+
+}
