01159ea4fce8ff7eef9bcfb4b6bcb35fb62bcef4
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-11 10:11:28 -0500

    feat: agregar servicio Publication para gestionar publicaciones con y sin archivos

---
+++ src/app/core/services/reaction/reaction-comments.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PublicationService {
+
+  private apiPublication = environment.apiPublication;
+
+  constructor(private httpClient: HttpClient) {}
+
+  public getPublicationsWithFilesPaginated(page: number = 0): Observable<any> {
+    return this.httpClient.get(`${this.apiPublication}/withfiles/paginated?page=${page}`);
+  }
+
+  public getAllPublicationwithoutFile(): Observable<any> {
+		return this.httpClient.get(`${this.apiPublication}/withoutfiles/paginated`);
+	}
+  
+  getPublicationById(idPublication: string): Observable<any> {
+    return this.httpClient.get(`${this.apiPublication}/details/${idPublication}`);
+  }
+  
+}
