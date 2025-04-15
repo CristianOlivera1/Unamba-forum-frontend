6ac9e84024e6a94f162f83b923cdf1b92e422e1a
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-15 18:37:26 -0500

    refactor: optimizar la función getTimeElapsed para incluir segundos y limpiar el código en TimeUtils

--- src/app/Utils/TimeElapsed.ts
+++ src/app/Utils/TimeElapsed.ts
@@ -1,21 +1,25 @@
 export class TimeUtils {
-    static getTimeElapsed(fechaRegistro: string): string {
-      const now = new Date();
-      const publishedDate = new Date(fechaRegistro);
-      const diffMs = now.getTime() - publishedDate.getTime(); // Diferencia en milisegundos
-  
-      const diffMinutes = Math.floor(diffMs / (1000 * 60));
-      if (diffMinutes < 60) {
-        return `${diffMinutes}m`; // Minutos
-      }
-  
-      const diffHours = Math.floor(diffMinutes / 60);
-      if (diffHours < 24) {
-        return `${diffHours}h`; // Horas
-      }
-  
-      const diffDays = Math.floor(diffHours / 24);
-      return `${diffDays}d`; // Días
+  static getTimeElapsed(fechaRegistro: string): string {
+    const now = new Date();
+    const publishedDate = new Date(fechaRegistro);
+    const diffMs = now.getTime() - publishedDate.getTime();
+
+    const diffSeconds = Math.floor(diffMs / 1000);
+    if (diffSeconds < 60) {
+      return `${diffSeconds}s`; // Segundos
     }
+
+    const diffMinutes = Math.floor(diffMs / (1000 * 60));
+    if (diffMinutes < 60) {
+      return `${diffMinutes}m`; // Minutos
+    }
+
+    const diffHours = Math.floor(diffMinutes / 60);
+    if (diffHours < 24) {
+      return `${diffHours}h`; // Horas
+    }
+
+    const diffDays = Math.floor(diffHours / 24);
+    return `${diffDays}d`; // Días
   }
-  
+}
--- src/app/Validators/CustomValidators.ts
+++ src/app/Validators/CustomValidators.ts
@@ -4,19 +4,19 @@ export class CustomValidators {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     return emailRegex.test(email) ? null : 'El email no tiene un formato válido.';
   }
-  
+
   validatePassword(password: string, confirmPassword: string): string | null {
     if (password.length < 8) return 'La contraseña debe tener al menos 8 caracteres.';
     if (password !== confirmPassword) return 'Las contraseñas no coinciden.';
     return null;
   }
-  
+
   validateField(field: string, message: string): string | null {
     return field.trim() ? null : message;
   }
-  
+
   validateCareer(career: string): string | null {
     return career ? null : 'Por favor selecciona una carrera válida.';
   }
-  
+
 }
