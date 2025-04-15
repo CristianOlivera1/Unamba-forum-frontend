13902ed8c6d3c9455103c21055e648c77210a521
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-15 18:37:18 -0500

    feat: agregar funcionalidad de modal de perfil al pasar el mouse en el componente Suggestion

--- src/app/pages/profile/components/suggestion/suggestion.component.ts
+++ src/app/pages/profile/components/suggestion/suggestion.component.ts
@@ -2,19 +2,77 @@ import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../../../../core/services/user/user.service';
 import { CommonModule } from '@angular/common';
+import { ProfileService } from '../../../../core/services/profile/profile.service';
+import { HoverAvatarComponent } from '../../../home/components/hover-avatar/hover-avatar.component';
 
 @Component({
   selector: 'app-suggestion',
-  imports: [CommonModule],
+  imports: [CommonModule, HoverAvatarComponent],
   templateUrl: './suggestion.component.html',
   styleUrl: './suggestion.component.css'
 })
 export class SuggestionComponent {
   @Input() suggestedUsers: any[] = [];
+  hoverProfileData: any = null;
+  hoverPosition = { top: 0, left: 0 };
+  isHoverModalVisible = false;
+  isHovering = false;
 
-  constructor(private userService: UserService, private router: Router) { } 
+  constructor(private router: Router, private profileService: ProfileService) { }
 
   navigateToProfileUser(idUsuario: string) {
     this.router.navigate(['/profile', idUsuario]);
+  }
+
+  showHoverModal(userId: string, event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (!target) {
+      console.error('El elemento objetivo no es un HTMLElement.');
+      return;
+    }
+
+    const rect = target.getBoundingClientRect();
+    this.isHoverModalVisible = true;
+    this.isHovering = true;
+
+    this.hoverPosition = {
+      top: rect.top + window.scrollY + rect.height + 5,
+      left: rect.left + window.scrollX + rect.width / 2 - 160
+    };
+
+    this.profileService.getUserProfileHover(userId).subscribe({
+      next: (response: any) => {
+        if (response.type === 'success') {
+          this.hoverProfileData = response.data;
+        }
+      },
+      error: (error) => {
+        console.error('Error al cargar la información del perfil:', error);
+      }
+    });
+  }
+
+  hideHoverModal(): void {
+    this.isHovering = false;
+    setTimeout(() => {
+      if (!this.isHovering) {
+        this.isHoverModalVisible = false;
+        this.hoverProfileData = null;
+      }
+    }, 200);
+  }
+  
+  onModalMouseEnter(): void {
+    this.isHovering = true;
+  }
+
+  onModalMouseLeave(): void {
+    this.isHovering = false;
+    setTimeout(() => {
+      if (!this.isHovering) {
+        this.isHoverModalVisible = false;
+        this.hoverProfileData = null;
+      }
+    }, 200);
   }
 }
