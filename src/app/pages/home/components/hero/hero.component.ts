40b31fbb5a1d1c694e4e0e9656cf23a42f51c1b8
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-15 18:36:35 -0500

    refactor: eliminar lógica de autenticación del componente Hero

--- src/app/pages/home/components/hero/hero.component.ts
+++ src/app/pages/home/components/hero/hero.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { TokenService } from '../../../../core/services/oauth/token.service';
+import {Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -9,17 +8,6 @@ import { TokenService } from '../../../../core/services/oauth/token.service';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
-export class HeroComponent implements OnInit {
-  isLoggedIn: boolean = false;
+export class HeroComponent  {
 
-  constructor(
-    private tokenService: TokenService,
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) {}
-
-  ngOnInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.isLoggedIn = !!this.tokenService.getToken();
-    }
-  }
 }
