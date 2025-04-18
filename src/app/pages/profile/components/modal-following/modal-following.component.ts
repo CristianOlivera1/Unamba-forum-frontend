63ecb6bbd13a1407c965ef0225bee9e50eb08b76
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-18 10:39:31 -0500

    feat: agregar funcionalidad de navegación y mejorar la interfaz de los modales de seguidores y seguidos

--- src/app/pages/profile/components/modal-following/modal-following.component.ts
+++ src/app/pages/profile/components/modal-following/modal-following.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modal-following',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './modal-following.component.html',
   styleUrl: './modal-following.component.css'
 })
 export class ModalFollowingComponent {
+  @Input() followings: any[] = [];
+  @Output() close = new EventEmitter<void>(); 
+  constructor( private router:Router){}
 
+  closeModal(): void {
+    this.close.emit();
+  }
+
+  navigateToProfileUser(idSeguido: string) {
+    this.router.navigate(['/profile', idSeguido]);
+    this.close.emit();
+  }
 }
