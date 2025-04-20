1f6ed37bd2304d793399c1014a94991769a8bda7
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-20 11:52:47 -0500

    feat: agregar modales para mostrar usuarios que reaccionaron y comentaron en publicaciones

--- src/app/pages/home/components/publication-with-files/publication-with-files.component.ts
+++ src/app/pages/home/components/publication-with-files/publication-with-files.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { PublicationService } from '../../../../core/services/publication/publication.service';
 import { CommonModule } from '@angular/common';
 import { TimeUtils } from '../../../../Utils/TimeElapsed';
@@ -15,31 +15,43 @@ import { ProfileService } from '../../../../core/services/profile/profile.servic
 import { HoverAvatarComponent } from '../hover-avatar/hover-avatar.component';
 import { PhotoSliderComponent } from '../photo-slider/photo-slider.component';
 import { RolService } from '../../../../core/services/rol/rol.service';
+import { ModalUsersByReactionTypeComponent } from '../modal-users-by-reaction-type/modal-users-by-reaction-type.component';
+import { FollowService } from '../../../../core/services/follow/follow.service';
+import { ReactionService } from '../../../../core/services/reaction/reaction-comments.service';
+import { ReactionPublicationService } from '../../../../core/services/reaction/reaction-publication.service';
+import { CommentPublicationService } from '../../../../core/services/CommentPublication/comment-publication.service';
+import { ModalUserCommentPublicationComponent } from '../modal-user-comment-publication/modal-user-comment-publication.component';
 
 @Component({
   selector: 'app-publication-with-files',
-  imports: [CommonModule,ReactionComponent,TotalsReactionCommentComponent,LoginModalComponent, CompleteInfoRegisterGoogleComponent,HoverAvatarComponent,PhotoSliderComponent],
+  imports: [CommonModule, ReactionComponent, TotalsReactionCommentComponent, LoginModalComponent, CompleteInfoRegisterGoogleComponent, HoverAvatarComponent, PhotoSliderComponent, ModalUsersByReactionTypeComponent, ModalUserCommentPublicationComponent],
   templateUrl: './publication-with-files.component.html',
   styleUrl: './publication-with-files.component.css'
 })
 export class PublicationWithFilesComponent implements OnInit {
   publications: any[] = [];
   currentUserId: string | null = null;
+  currentPublicationId: string | null = null;
+
   isLoginModalVisible$: any;
-  isInfoCompleteModalVisible$:any;
+  isInfoCompleteModalVisible$: any;
 
   hoverProfileData: any = null;
   hoverPosition = { top: 0, left: 0 };
   isHoverModalVisible = false;
   isHovering = false;
 
-  isPhotoSliderVisible = false; 
+  isPhotoSliderVisible = false;
   selectedPhotos: { tipo: string; rutaArchivo: string }[] = [];
   selectedPhotoIndex: number = 0;
-  
-  constructor(private publicationService: PublicationService,  private tokenService: TokenService,
-    private router: Router,private modalService: ModalLoginService,private modalInfoCompleteService:ModalInfoCompleteService ,private profileService: ProfileService,private rolService: RolService ) {}
-    
+
+  isReactionModalVisible: boolean = false;
+  reactionUsers: any[] = [];
+  reactionType: string = '';
+
+  constructor(private publicationService: PublicationService, private tokenService: TokenService, private commentPublicationService: CommentPublicationService,
+    private router: Router, private modalService: ModalLoginService, private modalInfoCompleteService: ModalInfoCompleteService, private profileService: ProfileService, private rolService: RolService, private reactionPublicationService: ReactionPublicationService) { }
+
   ngOnInit(): void {
     this.currentUserId = this.tokenService.getUserId();
     this.isLoginModalVisible$ = this.modalService.isLoginModalVisible$;
@@ -47,7 +59,7 @@ export class PublicationWithFilesComponent implements OnInit {
     this.loadPublications();
   }
 
- getTimeElapsedWrapper(fechaRegistro: string): string {
+  getTimeElapsedWrapper(fechaRegistro: string): string {
     return TimeUtils.getTimeElapsed(fechaRegistro);
   }
 
@@ -55,21 +67,27 @@ export class PublicationWithFilesComponent implements OnInit {
     this.publicationService.getPublicationsWithFilesPaginated(page).subscribe({
       next: (response: any) => {
         if (response.type === 'success') {
-          this.publications = response.data;
+          this.publications = response.data.map((publication: any) => ({
+            ...publication,
+            isReactionModalVisible: false,
+            reactionUsers: [],
+            reactionType: '',
+            hoverPosition: { top: 0, left: 0 }
+          }));
 
           // Obtener el rol de cada usuario en las publicaciones
-        this.publications.forEach((publication) => {
-          this.rolService.getRolByUserId(publication.idUsuario).subscribe({
-            next: (rolResponse: any) => {
-              if (rolResponse.type === 'success') {
-                publication.tipoRol = rolResponse.data.tipo;
+          this.publications.forEach((publication) => {
+            this.rolService.getRolByUserId(publication.idUsuario).subscribe({
+              next: (rolResponse: any) => {
+                if (rolResponse.type === 'success') {
+                  publication.tipoRol = rolResponse.data.tipo;
+                }
+              },
+              error: (error) => {
+                console.error('Error al obtener el rol del usuario:', error);
               }
-            },
-            error: (error) => {
-              console.error('Error al obtener el rol del usuario:', error);
-            }
+            });
           });
-        });
         } else {
           console.error('Error al cargar las publicaciones:', response.listMessage);
         }
@@ -80,76 +98,157 @@ export class PublicationWithFilesComponent implements OnInit {
     });
   }
 
-navigateToDetailPublication(idPublication: string) {
-  this.router.navigate(['/publication', idPublication]);
-}
-
-navigateToProfileUser(idUsuario: string) {
-  this.router.navigate(['/profile', idUsuario]);
-}
-
-openPhotoSlider(archivos: { tipo: string; rutaArchivo: string }[], index: number): void {
-  this.selectedPhotos = archivos;
-  this.selectedPhotoIndex = index; 
-  this.isPhotoSliderVisible = true;
-}
-
-closePhotoSlider(): void {
-  this.isPhotoSliderVisible = false;
-  this.selectedPhotos = [];
-  this.selectedPhotoIndex = 0; 
-}
-
-showHoverModal(userId: string, event: MouseEvent): void {
-  const target = event.target as HTMLElement;
-  if (!target) {
-    console.error('El elemento objetivo no es un HTMLElement.');
-    return;
+  navigateToDetailPublication(idPublication: string) {
+    this.router.navigate(['/publication', idPublication]);
   }
 
-  const rect = target.getBoundingClientRect();
-  this.isHoverModalVisible = true;
-  this.isHovering = true; 
+  navigateToProfileUser(idUsuario: string) {
+    this.router.navigate(['/profile', idUsuario]);
+  }
 
-  this.hoverPosition = {
-    top: rect.top + window.scrollY + rect.height + 5, 
-    left: rect.left + window.scrollX + rect.width / 2 - 160 
-  };
+  openPhotoSlider(archivos: { tipo: string; rutaArchivo: string }[], index: number): void {
+    this.selectedPhotos = archivos;
+    this.selectedPhotoIndex = index;
+    this.isPhotoSliderVisible = true;
+  }
 
-  this.profileService.getUserProfileHover(userId).subscribe({
-    next: (response: any) => {
-      if (response.type === 'success') {
-        this.hoverProfileData = response.data;
+  closePhotoSlider(): void {
+    this.isPhotoSliderVisible = false;
+    this.selectedPhotos = [];
+    this.selectedPhotoIndex = 0;
+  }
+
+  showHoverModal(userId: string, event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (!target) {
+      console.error('El elemento objetivo no es un HTMLElement.');
+      return;
+    }
+
+    const rect = target.getBoundingClientRect();
+    this.isHoverModalVisible = true;
+    this.isHovering = true;
+
+    this.hoverPosition = {
+      top: rect.top + window.scrollY + rect.height + 5,
+      left: rect.left + window.scrollX + rect.width / 2 - 160
+    };
+
+    this.profileService.getUserProfileHover(userId).subscribe({
+      next: (response: any) => {
+        if (response.type === 'success') {
+          this.hoverProfileData = response.data;
+        }
+      },
+      error: (error) => {
+        console.error('Error al cargar la información del perfil:', error);
       }
-    },
-    error: (error) => {
-      console.error('Error al cargar la información del perfil:', error);
-    }
-  });
-}
+    });
+  }
 
-hideHoverModal(): void {
-  this.isHovering = false; 
-  setTimeout(() => {
-    if (!this.isHovering) {
-      this.isHoverModalVisible = false;
-      this.hoverProfileData = null;
-    }
-  }, 200);
-}
+  hideHoverModal(): void {
+    this.isHovering = false;
+    setTimeout(() => {
+      if (!this.isHovering) {
+        this.isHoverModalVisible = false;
+        this.hoverProfileData = null;
+      }
+    }, 200);
+  }
 
-onModalMouseEnter(): void {
-  this.isHovering = true;
-}
+  openReactionHover(data: { event: MouseEvent; tipo: string }, publication: any): void {
+    const target = data.event.target as HTMLElement;
+    const rect = target.getBoundingClientRect();
 
-onModalMouseLeave(): void {
-  this.isHovering = false;
-  setTimeout(() => {
-    if (!this.isHovering) {
-      this.isHoverModalVisible = false;
-      this.hoverProfileData = null;
-    }
-  }, 200); 
-}
-  
+    publication.reactionType = data.tipo;
+    publication.isReactionModalVisible = true;
+
+    publication.hoverPosition = {
+      top: rect.top + window.scrollY - 5,
+      left: rect.left + window.scrollX + rect.width / 2 + 20
+    };
+
+    // Cargar los usuarios que reaccionaron
+    this.reactionPublicationService.getUsersByReactionType(publication.idPublicacion, data.tipo).subscribe({
+      next: (response: any) => {
+        if (response.type === 'success') {
+          publication.reactionUsers = response.data;
+        }
+      },
+      error: (error) => {
+        console.error('Error al cargar los usuarios por tipo de reacción:', error);
+      }
+    });
+  }
+
+  closeReactionHover(publication: any): void {
+    setTimeout(() => {
+      if (!this.isHovering) {
+        publication.isReactionModalVisible = false;
+        publication.reactionUsers = [];
+        publication.reactionType = '';
+      }
+    }, 200);
+  }
+  openCommentHover(data: { event: MouseEvent }, publication: any): void {
+    const target = data.event.target as HTMLElement;
+    const rect = target.getBoundingClientRect();
+    const modalWidth = 200;
+    const screenWidth = window.innerWidth;
+    const fitsOnRight = rect.left + rect.width / 2 + modalWidth / 2 <= screenWidth;
+
+    publication.isCommentModalVisible = true;
+    publication.commentHoverPosition = {
+      top: rect.top + window.scrollY + rect.height + 5,
+      left: fitsOnRight
+        ? rect.left + window.scrollX + rect.width / 2 - modalWidth / 2
+        : rect.left + window.scrollX - modalWidth / 2
+    };
+
+    this.commentPublicationService.getUsersWhoCommented(publication.idPublicacion).subscribe({
+      next: (response: any) => {
+        if (response.type === 'success') {
+          publication.usersComment = response.data;
+        } else {
+          console.error('API response error:', response.listMessage);
+        }
+      },
+      error: (error) => {
+        console.error('Error al cargar los usuarios que comentaron:', error);
+      }
+    });
+  }
+
+  closeCommentHover(publication: any): void {
+    setTimeout(() => {
+      if (!this.isHovering) {
+        publication.isCommentModalVisible = false;
+        publication.usersComment = [];
+      }
+    }, 200);
+  }
+
+  onModalMouseLeaveComment(publication: any): void {
+    this.isHovering = false;
+    this.closeCommentHover(publication);
+  }
+
+  onModalMouseEnter(): void {
+    this.isHovering = true;
+  }
+
+  onModalMouseLeave(): void {
+    this.isHovering = false;
+    setTimeout(() => {
+      if (!this.isHovering) {
+        this.isHoverModalVisible = false;
+        this.hoverProfileData = null;
+      }
+    }, 200);
+  }
+
+  onModalMouseLeaveReaction(publication: any): void {
+    this.isHovering = false;
+    this.closeReactionHover(publication);
+  }
 }
