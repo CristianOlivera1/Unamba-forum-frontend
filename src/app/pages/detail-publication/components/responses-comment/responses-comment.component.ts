446a0a1bb5a20d7da2ea14f814937e470cb34469
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-24 21:14:20 -0500

    feat: mejorar la funcionalidad de comentarios con selección de emoji y visualización de perfil en respuestas

--- src/app/pages/detail-publication/components/responses-comment/responses-comment.component.ts
+++ src/app/pages/detail-publication/components/responses-comment/responses-comment.component.ts
@@ -1,27 +1,93 @@
-import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, EventEmitter, Inject, Input, OnInit, Output, PLATFORM_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ResponseCommentService } from '../../../../core/services/commentPublication/response-comment.service';
+import { TokenService } from '../../../../core/services/oauth/token.service';
+import { ProfileService } from '../../../../core/services/profile/profile.service';
+import { HoverAvatarComponent } from '../../../home/components/hover-avatar/hover-avatar.component';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-responses-comment',
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule,HoverAvatarComponent],
   templateUrl: './responses-comment.component.html',
-  styleUrl: './responses-comment.component.css'
+  styleUrl: './responses-comment.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
 })
 export class ResponsesCommentComponent implements OnInit {
-  @Input() idComentario!: string; // ID del comentario al que se responde
-  @Input() idUsuario!: string; // ID del usuario logueado
-  @Output() close = new EventEmitter<void>(); // Evento para cerrar el componente
+  @Input() idComentario!: string;
+  @Input() idUsuario!: string;
+  @Output() close = new EventEmitter<void>();
+  @Input() nombreUsuario!: string;
+  responses: any[] = [];
+  responseContents: { [key: string]: string } = {};
+  newResponseContent: string = '';
+  selectedResponseId: string | null = null;
+  userProfile: any = null;
 
-  responses: any[] = []; // Lista de respuestas
-  responseContents: { [key: string]: string } = {}; // Almacena el contenido de cada respuesta
-  newResponseContent: string = ''; // Almacena el contenido de una nueva respuesta
-  selectedResponseId: string | null = null; // ID de la respuesta seleccionada para responder
-  constructor(private responseCommentService: ResponseCommentService) {}
+  showEmojiPicker: boolean = false;
+
+  hoverProfileData: any = null;
+  hoverPosition = { top: 0, left: 0 };
+  isHoverModalVisible = false;
+  isHovering = false;
+
+  constructor(private responseCommentService: ResponseCommentService, @Inject(PLATFORM_ID) private platformId: Object, private tokenService: TokenService,
+    private profileService: ProfileService, private router:Router) { }
 
   ngOnInit(): void {
+    this.loadUserProfile();
+    if (isPlatformBrowser(this.platformId)) {
+      import('emoji-picker-element');
+    }
+    this.newResponseContent = `@${this.nombreUsuario} `;
     this.loadResponses();
+  }
+  toggleEmojiPicker(): void {
+    this.showEmojiPicker = !this.showEmojiPicker;
+  }
+  addEmoji(event: any): void {
+    const emoji = event.detail.unicode; // Obtener el emoji seleccionado
+    if (this.selectedResponseId) {
+      this.responseContents[this.selectedResponseId] += emoji;
+    } else {
+      this.newResponseContent += emoji;
+    }
+    this.showEmojiPicker = false; // Cierra el selector después de seleccionar un emoji
+  }
+  loadUserProfile(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const userId = this.extractUserIdFromToken();
+      if (userId) {
+        this.profileService.getProfileByUserId(userId).subscribe(
+          (response) => {
+            if (response.type === 'success') {
+              this.userProfile = response.data;
+              resolve();
+            } else {
+              reject(response.listMessage);
+            }
+          },
+          (error) => {
+            console.error('Error en la solicitud del perfil:', error);
+            reject(error);
+          }
+        );
+      } else {
+        reject('No se pudo extraer el ID del usuario del token.');
+      }
+    });
+  }
+
+  extractUserIdFromToken(): string | null {
+    const token = this.tokenService.getToken();
+    if (token) {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      return payload.idUsuario || null;
+    }
+    return null;
   }
 
   loadResponses(): void {
@@ -41,25 +107,46 @@ export class ResponsesCommentComponent implements OnInit {
   }
   private flattenResponses(responses: any[]): any[] {
     const flattened: any[] = [];
-  
+
     responses.forEach(response => {
-      flattened.push(response); // Agregar la respuesta actual
+      flattened.push(response);
       if (response.respuestasHijas && response.respuestasHijas.length > 0) {
         // Aplanar las respuestas hijas y agregarlas al array
         flattened.push(...this.flattenResponses(response.respuestasHijas));
       }
     });
-  
+
     return flattened;
   }
   addResponse(responseId?: string): void {
-    const content = responseId ? this.responseContents[responseId] : this.newResponseContent;
-  
+    let content = responseId ? this.responseContents[responseId] : this.newResponseContent;
+
     if (!content?.trim()) {
       console.error('La respuesta no puede estar vacía.');
       return;
     }
-  
+    // Validar que el contenido no sea solo la mención
+    if (responseId) {
+      const mention = `@${this.responses.find(r => r.idRespuesta === responseId)?.nombreCompleto} `;
+      if (content.trim() === mention.trim()) {
+        console.error('No puedes enviar solo la mención.');
+        return;
+      }
+      if (!content.startsWith(mention)) {
+        content = mention + content; // Agregar la mención al inicio si no está
+      }
+    } else {
+      const mention = `@${this.nombreUsuario} `;
+      if (content.trim() === mention.trim()) {
+        console.error('No puedes enviar solo la mención.');
+        return;
+      }
+      if (!content.startsWith(mention)) {
+        content = mention + content; // Agregar la mención al inicio si no está
+      }
+    }
+
+
     const formData = new FormData();
     if (responseId) {
       formData.append('idRespuestaPadre', responseId); // Responder a una respuesta
@@ -69,18 +156,18 @@ export class ResponsesCommentComponent implements OnInit {
 
     formData.append('idUsuario', this.idUsuario);
     formData.append('contenido', content);
-  
+
     this.responseCommentService.addResponse(formData).subscribe({
       next: (response: any) => {
         if (response.type === 'success') {
           console.log('Respuesta agregada correctamente.');
           if (responseId) {
-            delete this.responseContents[responseId]; // Limpiar el contenido de la respuesta
+            delete this.responseContents[responseId];
           } else {
-            this.newResponseContent = ''; // Limpiar el contenido del comentario
+            this.newResponseContent = '';
           }
-          this.selectedResponseId = null; // Reiniciar el ID de la respuesta seleccionada
-          this.loadResponses(); // Recargar las respuestas
+          this.selectedResponseId = null;
+          this.loadResponses();
         } else {
           console.error('Error al agregar la respuesta:', response.listMessage);
         }
@@ -90,10 +177,76 @@ export class ResponsesCommentComponent implements OnInit {
       }
     });
   }
-  handleReplyToResponse(responseId: string): void {
-    this.selectedResponseId = responseId; // Establece el ID de la respuesta seleccionada
+
+  handleReplyToResponse(response: any): void {
+    this.selectedResponseId = response.idRespuesta;
+    this.responseContents[response.idRespuesta] = `@${response.nombreCompleto} `;
+  }
+  
+  
+  showHoverModal(userId: string, event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (!target) {
+      console.error('El elemento objetivo no es un HTMLElement.');
+      return;
+    }
+
+    const rect = target.getBoundingClientRect();
+    this.isHoverModalVisible = true;
+    this.isHovering = true;
+
+    this.hoverPosition = {
+      top: rect.top + window.scrollY + rect.height - 40,
+      left: rect.left + window.scrollX + rect.width / 2 +30
+    };
+
+    this.profileService.getUserProfileHover(userId).subscribe({
+      next: (response: any) => {
+        if (response.type === 'success') {
+          this.hoverProfileData = response.data;
+        }
+      },
+      error: (error) => {
+        console.error('Error al cargar la información del perfil:', error);
+      }
+    });
+  }
+
+  hideHoverModal(): void {
+    this.isHovering = false;
+    setTimeout(() => {
+      if (!this.isHovering) {
+        this.isHoverModalVisible = false;
+        this.hoverProfileData = null;
+      }
+    }, 200);
+  }
+  onModalMouseEnter(): void {
+    this.isHovering = true;
+  }
+
+  onModalMouseLeave(): void {
+    this.isHovering = false;
+    setTimeout(() => {
+      if (!this.isHovering) {
+        this.isHoverModalVisible = false;
+        this.hoverProfileData = null;
+      }
+    }, 200);
   }
   closeResponses(): void {
-    this.close.emit(); // Emitir evento para cerrar el componente
+    this.close.emit();
+  }
+
+  enforceMentionForComment(): void {
+    const mention = `@${this.nombreUsuario} `;
+
+  }
+  enforceMention(responseId: string, nombreCompleto: string): void {
+    const mention = `@${nombreCompleto} `;
+  }
+
+  navigateToProfileUser(idUsuario: string){
+    this.router.navigate(['/profile',idUsuario])
   }
 }
