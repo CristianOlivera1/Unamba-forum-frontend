29d25ff95b2acd3632a17ba091dd02fdea9a0286
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-29 18:04:41 -0500

    feat: agregar componente para editar publicaciones con su respectiva plantilla y estilos

---
+++ src/app/pages/home/edit-publication/edit-publication.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+
+@Component({
+  selector: 'app-edit-publication',
+  imports: [HeaderComponent],
+  templateUrl: './edit-publication.component.html',
+  styleUrl: './edit-publication.component.css'
+})
+export class EditPublicationComponent {
+
+}
