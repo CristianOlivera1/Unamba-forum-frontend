0d37d4a74a9b99eba019dadaee5d9d53679858fa
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-29 18:05:52 -0500

    feat: eliminar la tubería ReplaceNewlinesPipe y sus pruebas asociadas

--- src/app/shared/pipes/replace-newlines.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ReplaceNewlinesPipe } from './replace-newlines.pipe';
-
-describe('ReplaceNewlinesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ReplaceNewlinesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
