e317fc373c41bf1b686d1b1c052c4ad8f65c27eb
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-29 18:05:58 -0500

    feat: comentar el cálculo de segundos en la función getTimeElapsed

--- src/app/Utils/TimeElapsed.ts
+++ src/app/Utils/TimeElapsed.ts
@@ -4,10 +4,10 @@ export class TimeUtils {
     const publishedDate = new Date(fechaRegistro);
     const diffMs = now.getTime() - publishedDate.getTime();
 
-    const diffSeconds = Math.floor(diffMs / 1000);
-    if (diffSeconds < 60) {
-      return `${diffSeconds}s`; // Segundos
-    }
+    // const diffSeconds = Math.floor(diffMs / 1000);
+    // if (diffSeconds < 60) {
+    //   return `${diffSeconds}s`; // Segundos
+    // }
 
     const diffMinutes = Math.floor(diffMs / (1000 * 60));
     if (diffMinutes < 60) {
