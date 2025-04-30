d84c010a61eaa79a8aa5ea3bb0d6f40c56494dfb
CristianOlivera1 <221181@unamba.edu.pe>
2025-04-30 18:39:29 -0500

    feat: agregar propiedad 'fijada' a la interfaz Publication

--- src/app/core/interfaces/publication.ts
+++ src/app/core/interfaces/publication.ts
@@ -1,23 +1,25 @@
 export interface Publication {
-    idPublicacion: string;
-    idUsuario: string;
-    titulo: string;
-    contenido: string;
-    archivos: { tipo: string; rutaArchivo: string }[]; 
-    avatar: string; 
-    nombreCompleto: string;
-    nombreCarrera: string;
-    nombreCategoria: string; 
-    fechaRegistro: string; 
-    isReactionModalVisible: boolean;
-    reactionUsers: any[];
-    reactionType: string;
-    hoverPosition: { top: number; left: number };
-    tipoRol?: string;
-    reacciones: any[]; 
-    isCommentModalVisible?: boolean;
-    commentHoverPosition?: { top: number; left: number };
-    usersComment?: any[];
-    totalComentarios?: number; 
-      isDropdownVisible?: boolean;
-  }
+  idPublicacion: string;
+  idUsuario: string;
+  titulo: string;
+  contenido: string;
+  archivos: { tipo: string; rutaArchivo: string }[];
+  avatar: string;
+  nombreCompleto: string;
+  nombreCarrera: string;
+  nombreCategoria: string;
+  fechaRegistro: string;
+  isReactionModalVisible: boolean;
+  reactionUsers: any[];
+  reactionType: string;
+  hoverPosition: { top: number; left: number };
+  tipoRol?: string;
+  reacciones: any[];
+  isCommentModalVisible?: boolean;
+  commentHoverPosition?: { top: number; left: number };
+  usersComment?: any[];
+  totalComentarios?: number;
+  isDropdownVisible?: boolean;
+  fijada?: boolean;
+
+}
