b0c14d2d88e1c72b648833e95e809fd1de51990e
CristianOlivera1 <221181@unamba.edu.pe>
2025-05-09 18:07:38 -0500

    feat: agregar componente de filtro de carrera con HTML, CSS y pruebas

---
+++ src/app/pages/career/components/filter-career/filter-career.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-career',
+  imports: [],
+  templateUrl: './filter-career.component.html',
+  styleUrl: './filter-career.component.css'
+})
+export class FilterCareerComponent {
+
+}
