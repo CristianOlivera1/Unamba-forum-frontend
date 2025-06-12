597769259a11e506f19e3410cdc8512bda6f0b9b
cristianolivera1 <221181@unamba.edu.pe>
2025-06-12 09:31:30 -0500

    feat(publication): enhance publication component with filter handling and data binding

--- src/app/shared/components/publication/publication.component.ts
+++ src/app/shared/components/publication/publication.component.ts
@@ -5,21 +5,39 @@ import { TokenService } from '../../../core/services/oauth/token.service';
 import { PublicationWithoutFilesComponent } from '../../../pages/home/components/publication-without-files/publication-without-files.component';
 import { FilterComponent } from '../../../pages/home/components/filter/filter.component';
 import { PublicationWithFilesComponent } from '../../../pages/home/components/publication-with-files/publication-with-files.component';
+import { PublicationService } from '../../../core/services/publication/publication.service';
 
 @Component({
   selector: 'app-publication',
   standalone: true,
-  imports: [CommonModule,WelcomeComponent,PublicationWithoutFilesComponent,FilterComponent,PublicationWithFilesComponent],
+  imports: [CommonModule, WelcomeComponent, PublicationWithoutFilesComponent, FilterComponent, PublicationWithFilesComponent],
   templateUrl: './publication.component.html',
   styleUrl: './publication.component.css'
 })
 export class PublicationComponent {
- 
+  publicationsWithFiles: any[] = [];
+  publicationsWithoutFiles: any[] = [];
   isLoggedIn: boolean = false;
+isFiltered = false;
 
-  constructor(private tokenService: TokenService) {}
+  constructor(private tokenService: TokenService, private publicationService:PublicationService) { }
   ngOnInit(): void {
     this.isLoggedIn = !!this.tokenService.getToken();
   }
-  
+
+onFilterChanged(filter: { idCarrera: string, idCategoria: string }) {
+  this.isFiltered = !!(filter.idCarrera || filter.idCategoria);
+  this.publicationService.filterPublications(filter.idCategoria, filter.idCarrera).subscribe(res => {
+    if (res.type === 'success') {
+      this.publicationsWithFiles = Array.isArray(res.publicacionesConArchivos) ? res.publicacionesConArchivos : [];
+      this.publicationsWithoutFiles = Array.isArray(res.publicacionesSinArchivos) ? res.publicacionesSinArchivos : [];
+    } else {
+      this.publicationsWithFiles = [];
+      this.publicationsWithoutFiles = [];
+    }
+  }, () => {
+    this.publicationsWithFiles = [];
+    this.publicationsWithoutFiles = [];
+  });
+}
 }
