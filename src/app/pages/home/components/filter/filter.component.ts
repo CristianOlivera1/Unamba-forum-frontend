e3491b3648e5a107552accc6a5b9fb4397af6d3f
cristianolivera1 <221181@unamba.edu.pe>
2025-06-12 09:30:59 -0500

    feat(filter): implement career and category filtering with data retrieval

--- src/app/pages/home/components/filter/filter.component.ts
+++ src/app/pages/home/components/filter/filter.component.ts
@@ -1,17 +1,72 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { CareerService } from '../../../../core/services/career/career.service';
+import { CategoryService } from '../../../../core/services/category/category.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-filter',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit {
   isDragging = false;
   startX = 0;
   scrollLeft = 0;
 
+  careers: any[] = [];
+  categories: any[] = [];
+
+  selectedCareer: string = '';
+  selectedCategory: string = '';
+
+    @Output() filterChanged = new EventEmitter<{ idCarrera: string, idCategoria: string }>();
+
   @ViewChild('scrollContainer', { static: true }) scrollContainer!: ElementRef;
+
+    constructor(
+    private careerService: CareerService,
+    private categoryService: CategoryService
+  ) {}
+
+ngOnInit() {
+  this.careerService.getAllCareer().subscribe({
+    next: res => {
+      this.careers = (res && res.data) ? res.data : [];
+    },
+    error: () => {
+      this.careers = [];
+    }
+  });
+  this.categoryService.getAllCategory().subscribe({
+    next: res => {
+      this.categories = (res && res.data) ? res.data : [];
+    },
+    error: () => {
+      this.categories = [];
+    }
+  });
+  setTimeout(() => {
+    this.emitFilter();
+  }, 0);
+}
+ onCareerSelect(idCarrera: string) {
+    this.selectedCareer = idCarrera;
+    this.emitFilter();
+  }
+
+  onCategorySelect(event: any) {
+    this.selectedCategory = event.target.value;
+    this.emitFilter();
+  }
+
+  emitFilter() {
+    this.filterChanged.emit({
+      idCarrera: this.selectedCareer,
+      idCategoria: this.selectedCategory
+    });
+  }
+
   startDrag(event: MouseEvent | TouchEvent) {
     this.isDragging = true;
 
