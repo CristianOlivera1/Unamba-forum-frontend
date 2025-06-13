780efc4f87b6d7625fe6975ea6c5508152821150
cristianolivera1 <221181@unamba.edu.pe>
2025-06-13 18:31:45 -0500

    feat(summary): enhance summary component with GSAP animations and update user counts

--- src/app/shared/components/summary/summary.component.ts
+++ src/app/shared/components/summary/summary.component.ts
@@ -1,8 +1,9 @@
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SummaryService } from '../../../core/services/summary/summary.service';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { TokenService } from '../../../core/services/oauth/token.service';
+import gsap from "gsap";
 
 @Component({
   selector: 'app-summary',
@@ -11,9 +12,16 @@ import { TokenService } from '../../../core/services/oauth/token.service';
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.css'
 })
-export class SummaryComponent implements OnInit {
+export class SummaryComponent implements OnInit,AfterViewInit {
   total: any;
   isLoggedIn: boolean = false;
+
+    usersCount = 0;
+  publicationsCount = 0;
+  careersCount = 0;
+ @ViewChild('usersRef') usersRef!: ElementRef;
+  @ViewChild('publicationsRef') publicationsRef!: ElementRef;
+  @ViewChild('careersRef') careersRef!: ElementRef;
 
   constructor(
     private route: ActivatedRoute,
@@ -28,20 +36,56 @@ export class SummaryComponent implements OnInit {
     }
   }
 
-  loadSummaryData(): void {
-    this.summaryService.getTotalCareerPublicationUser().subscribe({
-      next: (response: any) => {
-        if (response.type === 'success') {
-          this.total = response.data;
+loadSummaryData(): void {
+  this.summaryService.getTotalCareerPublicationUser().subscribe({
+    next: (response: any) => {
+      if (response.type === 'success') {
+        this.total = response.data;
 
+        if (isPlatformBrowser(this.platformId)) {
+          gsap.to(this, {
+            duration: 1.5,
+            usersCount: this.total?.totalUsers || 0,
+            roundProps: "usersCount",
+            ease: "power1.out"
+          });
+          gsap.to(this, {
+            duration: 1.5,
+            publicationsCount: this.total?.totalPublications || 0,
+            roundProps: "publicationsCount",
+            ease: "power1.out"
+          });
+          gsap.to(this, {
+            duration: 1.5,
+            careersCount: this.total?.totalCareers || 0,
+            roundProps: "careersCount",
+            ease: "power1.out"
+          });
         } else {
-          console.error('Error al cargar las carreras:', response.listMessage);
-          alert('No se pudieron cargar las carreras.');
+          // SSR: asigna directamente
+          this.usersCount = this.total?.totalUsers || 0;
+          this.publicationsCount = this.total?.totalPublications || 0;
+          this.careersCount = this.total?.totalCareers || 0;
         }
-      },
-      error: (error: any) => {
-        console.error('Error en la solicitud:', error);
+      } else {
+        console.error('Error al cargar las carreras:', response.listMessage);
+        alert('No se pudieron cargar las carreras.');
       }
-    });
+    },
+    error: (error: any) => {
+      console.error('Error en la solicitud:', error);
+    }
+  });
+}
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Blur para users
+      gsap.fromTo(this.usersRef.nativeElement, { filter: "blur(3px)" }, { filter: "blur(0px)", duration: 1.5});
+      // Blur para publications
+      gsap.fromTo(this.publicationsRef.nativeElement, { filter: "blur(3px)" }, { filter: "blur(0px)", duration: 1.5 });
+      // Blur para careers
+      gsap.fromTo(this.careersRef.nativeElement, { filter: "blur(3px)" }, { filter: "blur(0px)", duration: 1.5 });
+    }
   }
 }
